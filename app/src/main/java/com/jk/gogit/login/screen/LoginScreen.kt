f7ac8b917dd52c763c71e16fe25a0fe80384ffcb
Jitendra <jk2praj@gmail.com>
2024-04-09 21:14:39 +0800

    Moved BL to viewmodel

--- app/src/main/java/com/jk/gogit/login/screen/LoginScreen.kt
+++ app/src/main/java/com/jk/gogit/login/screen/LoginScreen.kt
@@ -1,148 +1,161 @@
 package com.jk.gogit.login.screen
 
-import com.hoppers.networkmodule.network.AuthManager
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.OAuthCredential
 import com.google.firebase.auth.OAuthProvider
-import com.google.firebase.auth.internal.zzab
 import com.jk.gogit.MainActivity
 import com.jk.gogit.R
+import com.jk.gogit.components.Page
 import com.jk.gogit.components.localproviders.LocalNavController
 import com.jk.gogit.login.AuthRequestModel
+import com.jk.gogit.login.AuthViewModel
+import com.jk.gogit.login.AuthenticationState
 import com.jk.gogit.navigation.AppScreens
-import com.jk.gogit.profile.model.RawUserInfo
 import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
 fun LoginScreen() {
-
     val activity = LocalContext.current as MainActivity
     val provider = OAuthProvider.newBuilder("github.com")
     provider.setScopes(AuthRequestModel().generate().scopes)
 
     val localNavController = LocalNavController.current
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Spacer(modifier = Modifier.height(20.dp))
 
-        Image(
-            modifier = Modifier
-                .heightIn(max = 300.dp)
-                .fillMaxWidth(),
-            contentDescription = "App Icon",
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            alignment = Alignment.Center
-        )
+    val authViewModel: AuthViewModel = koinViewModel<AuthViewModel>()
 
-        Button(
-            onClick = {
-                activity.lifecycleScope.launch {
-                    signInWithGithub(
-                        activity = activity,
-                        provider = provider
-                    ) { accessToken, login ->
-                        val authManager = com.hoppers.networkmodule.network.AuthManager
-                        authManager.saveAccessToken(accessToken, login)
-                        localNavController.navigate(AppScreens.FEED.route)
+    val authenticationState by authViewModel.authenticationState.collectAsState()
+    Page {
+        when (authenticationState) {
+            AuthenticationState.Initial -> {
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp),
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .heightIn(max = 300.dp)
+                            .align(Alignment.CenterHorizontally),
+                        contentDescription = "App Icon",
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground),
+                        painter = painterResource(id = R.mipmap.ic_launcher_foreground),
+                        alignment = Alignment.Center,
+                    )
+
+                    Spacer(modifier = Modifier.height(50.dp))
+
+                    Button(
+                        onClick = {
+                            activity.lifecycleScope.launch {
+                                authViewModel.signInWithGithub(
+                                    activity = activity,
+                                    provider = provider
+                                )
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                    ) {
+                        Text(text = stringResource(id = R.string.sign_in))
+                    }
+                    Spacer(modifier = Modifier.height(100.dp))
+                    val annotatedString = buildAnnotatedString {
+                        pushStyle(
+                            style = ParagraphStyle(textAlign = TextAlign.Center)
+                        )
+                        append("Your login indicates acceptance of our ")
+                        pushStringAnnotation(
+                            tag = "PrivacyPolicy",
+                            annotation = "https://www.example.com/privacy_policy"
+                        ) // Replace the URL with your actual Privacy Policy URL
+                        withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
+                            append("Privacy Policy")
+                        }
+                        pop()
+                    }
+
+                    ClickableText(
+                        modifier = Modifier
+                            .widthIn(max = 200.dp)
+                            .align(Alignment.CenterHorizontally),
+                        text = annotatedString,
+                        onClick = { offset ->
+                            annotatedString.getStringAnnotations(
+                                tag = "PrivacyPolicy",
+                                start = offset,
+                                end = offset
+                            )
+                                .firstOrNull()?.let { annotation ->
+                                    // Handle click on Privacy Policy
+                                    // You can navigate to the Privacy Policy page or open a web browser with the URL
+                                    // Example:
+                                    // val privacyPolicyUrl = annotation.item
+                                    // openPrivacyPolicyPage(privacyPolicyUrl)
+                                }
+                        },
+                        style = MaterialTheme.typography.bodySmall,
+                        maxLines = 2
+                    )
+
+
+                }
+                // Other UI elements
+
+            }
+
+            AuthenticationState.Authenticated ->
+                localNavController.navigate(AppScreens.USERPROFILE.route) {
+                    popUpTo(AppScreens.LOGIN.route) {
+                        inclusive = true
                     }
                 }
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 8.dp)
-        ) {
-            Text(text = stringResource(id = R.string.sign_in))
-        }
 
-        TextButton(
-            onClick = { /* Handle skip */ },
-            modifier = Modifier
-                .align(Alignment.End)
-                .padding(top = 16.dp)
-        ) {
-            Text(text = stringResource(id = R.string.skip_login_now))
-        }
 
-        Text(
-            text = stringResource(id = R.string.terms_condition),
-            modifier = Modifier.padding(top = 16.dp),
-            color = Color.White
-        )
+            AuthenticationState.AuthenticationFailed -> {
+                // Handle authentication failure
+                // You may display an error message to the user
+            }
+
+            AuthenticationState.Loading ->
+                // Show loader
+                CircularProgressIndicator(modifier = Modifier.size(50.dp))
+
+        }
     }
 }
-
-private fun signInWithGithub(
-    activity: MainActivity,
-    provider: OAuthProvider.Builder,
-    onSuccessfulSignIn: (String, String) -> Unit
-) {
-
-    val firebaseAuth = FirebaseAuth.getInstance()
-    val currentUser = firebaseAuth.currentUser
-    if (currentUser != null && !com.hoppers.networkmodule.network.AuthManager.getAccessToken()
-            .isNullOrEmpty()
-    ) {
-        return
-    }
-    val pendingResultTask = firebaseAuth.pendingAuthResult
-    // There's something already here! Finish the sign-in for your user.
-    pendingResultTask?.addOnSuccessListener { authResult ->
-        val accessToken = (authResult.credential as OAuthCredential).accessToken
-        val user = authResult.user?.providerData?.last()?.uid
-        //  onSuccessfulSignIn(accessToken.toString())
-    }?.addOnFailureListener {
-        // Handle failure.
-    } ?: // There's no pending result so you need to start the sign-in flow.
-    firebaseAuth
-        .startActivityForSignInWithProvider(activity, provider.build())
-        .addOnSuccessListener { authResult ->
-            // User is signed in.
-            // IdP data available in
-            val profileJson = (authResult.user?.providerData?.last() as zzab).zzb()
-            val json = Json.parseToJsonElement(profileJson.toString()).jsonObject
-            val rawUserInfoString = json["rawUserInfo"]?.jsonPrimitive?.content ?: ""
-
-            // Parse the rawUserInfo JSON
-            val rawUserInfoJson = Json.parseToJsonElement(rawUserInfoString).jsonObject
-
-            val rawUserInfo: RawUserInfo = Json.decodeFromString(rawUserInfoJson.toString())
-            val login = rawUserInfo.login
-            // The OAuth access token can also be retrieved:
-            val accessToken = (authResult.credential as OAuthCredential).accessToken
-            onSuccessfulSignIn(accessToken.toString(), login)
-        }
-        .addOnFailureListener {}
-}
-
-
