b06ba193123d280e94f8f096225748cf879a9163
Jitendra <jk2praj@gmail.com>
2024-05-16 15:47:06 +0800

    removed unused code

--- app/src/main/java/com/jk/gogit/navigation/AppScreens.kt
+++ app/src/main/java/com/jk/gogit/navigation/AppScreens.kt
@@ -2,14 +2,12 @@ package com.jk.gogit.navigation
 
 sealed class AppScreens(val route: String) {
     data object LOGIN : AppScreens(ScreenName.LOGIN_SCREEN)
-    data object FEED : AppScreens(ScreenName.FEED_SCREEN)
     data object USERPROFILE : AppScreens(ScreenName.USER_PROFILE_SCREEN)
     data object USERLIST : AppScreens(ScreenName.USER_LIST_SCREEN)
     data object REPOLIST : AppScreens(ScreenName.REPO_LIST_SCREEN)
     data object SEARCH : AppScreens(ScreenName.SEARCH_SCREEN)
     data object REPODETAIL : AppScreens(ScreenName.REPO_DETAIL_SCREEN)
     data object  PULLREQUESTS : AppScreens(ScreenName.PULL_REQUESTS_SCREEN)
-    //data object STARREDREPOLIST : AppScreens(ScreenName.STARREDREPOLIST)
     data object REPOTREESCREEN : AppScreens(ScreenName.REPOTREESCREEN)
     data object ORGLIST : AppScreens(ScreenName.ORGLIST_SCREEN)
     data object ORGDETAIL : AppScreens(ScreenName.ORGDETAIL_SCREEN)
@@ -20,7 +18,6 @@ sealed class AppScreens(val route: String) {
     class ScreenName {
         companion object {
             const val LOGIN_SCREEN: String = "login_screen"
-            const val FEED_SCREEN: String = "feed_screen"
             const val USER_PROFILE_SCREEN: String = "user_profile_screen"
             const val USER_LIST_SCREEN: String = "user_list_screen"
             const val REPO_LIST_SCREEN: String = "repo_list_screen"
